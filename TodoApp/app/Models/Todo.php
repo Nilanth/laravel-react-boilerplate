3219a9fdef1481d9655dc8ffce2592ac35e01ca4
Gormm1980 <guillermosantamariarodrigo@hotmail.com>
2021-09-08 16:59:01 +0000

    todo models create

--- TodoApp/app/Models/Todo.php
+++ TodoApp/app/Models/Todo.php
@@ -8,4 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class Todo extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'user_id',
+        'completed',
+        'active'
+    ];
+
+    /**
+     * Belongs To Relation - Eloquent Relation For Todo user
+     *
+     * @return void
+     */
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
 }
